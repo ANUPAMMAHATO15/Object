fb756e11078b2ecdffbf420265b20e99cec564cd
ANUPAMMAHATO15 <91774391+ANUPAMMAHATO15@users.noreply.github.com>
2021-11-26 21:28:54 +0530

    Create Assignment.js

---
+++ Assignment.js
@@ -0,0 +1,26 @@
+var person ={
+    firstName: "Anupam",
+    lastName: "Mahato",
+    officeAddress: "Bangalore"
+};
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.officeAddress);
+
+var person ={
+    firstName: "Puja",
+    lastName: "Mahato",
+    officeAddress: "Hyderabad"
+};
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.officeAddress);
+
+var person ={
+    firstName: "Supriti",
+    lastName: "Mahato",
+    officeAddress: "Mumbai"
+};
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.officeAddress);
